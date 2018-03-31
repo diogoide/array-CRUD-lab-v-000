2348f44dcec917d2390aa4ad65edfa8907a6d773
Diogo Ide <diogoide@gmail.com>
2018-03-31 22:54:18 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,11 +9,13 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
-  array.push(element) 
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array = ["I", "am", "really", "learning"]
+  element = "wow"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
